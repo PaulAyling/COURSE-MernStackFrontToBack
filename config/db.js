2753d543d9710537baacaca1cdcf77b49117b4cc
Paul AYling <jimmyyukka@gmail.com>
2019-05-24 09:49:19 +0200

    authentification
    can now communicate and add users to the database

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db,{
+        useNewUrlParser: true ,
+        useCreateIndex: true
+        });
+    
+    console.log('mongo DB connected .......');
+  } catch (err) {
+    console.error(err.message);
+    //exit process with faliure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
